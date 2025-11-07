1ffc655a42b44aa5b59ad7959bf02b025746feac
Raman attri <raman.kumar.1232004@gmail.com>
2025-11-07 23:17:34 +0530

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
-let age = +prompt("Enter the age");
-if(age>=18){
-    console.log("Eligible");
-}else{
-    console.log("Not Eligible");
-}
+// let age = prompt("Enter the age");
+
+// if(age===null){
+//     console.error("You have canceled it.");
+// }else{
+//     if(age.trim()===""){
+//         console.log("Please write something...");
+//     }else{
+//       age = Number(age);
+//       if(isNaN(age)){
+//         console.error("Please enter a number");
+//       }else{
+//         if(age<0){
+//             console.log("Not consider");
+//         } else if(age>=18){
+//             console.log("You can vote");
+//         }else{
+//             console.log("You cant vote");
+//         }
+//       }
+//     }
+// }
+
+
+
+// for(let i = 1 ; i<11;i++){
+//     console.log(`5 x ${i} = ${5*i}`);
+// }
+
+// for(let i = 1 ;  i<11 ;  i++){
+//     console.log(`38 x ${i} = ${38*i}`);
+// }
+
+// let count = 0;
+// for(let i = 1  ; i<16 ; i++){
+//     if(i>8){
+//         count++;
+//         console.log(i);
+//     }
+// }
+// console.log(`Total count is ${count}`);
+
+
+// let password = "qwertyRaman";
+// let pass = prompt("Enter the password..");
+
+// if(pass===null){
+//     console.error("You canceled it");
+// }else{
+//     if(pass.trim()===""){
+//         console.error("Please enter something");
+//     }else{
+//         if(pass===password){
+//             console.log("Matched");
+//         }else{
+//             console.log("Not matched");
+//         }
+//     }
+// }
+
